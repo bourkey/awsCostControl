e80acee02a221651cf66864dbecb99eb75e0a423
Nick Bourke <nbourke@tenable.com>
2019-02-22 09:32:14 +1100

    comment code more approriate

--- awsStartInstances.py
+++ awsStartInstances.py
@@ -1,12 +1,16 @@
+# import reqs
 import boto3
 
+# Set Approriate Region
 region = 'ap-southeast-2'
 
+# Lamba Handler
 def lambda_handler(event, context):
     ec2client = boto3.client('ec2', region_name=region)
     ec2 = boto3.resource('ec2', region_name=region)
     ec2RunningInstancesIDs = []
     instances = ec2.instances.filter(
+        # Filter Instances based On Tag, Check Current State
         Filters=[{'Name': 'tag:startup', 'Values': ['true']}, {'Name': 'instance-state-name', 'Values': ['stopped']}])
     if not instances:
         exit(1)
@@ -14,4 +18,3 @@ def lambda_handler(event, context):
         ec2RunningInstancesIDs.append(instance.id)
     ec2client.start_instances(InstanceIds=ec2RunningInstancesIDs)
 
-    
--- awsStopInstances.py
+++ awsStopInstances.py
@@ -1,5 +1,7 @@
+# import reqs
 import boto3
 
+# Set Approriate Region
 region = 'ap-southeast-2'
 
 def lambda_handler(event, context):
@@ -7,6 +9,7 @@ def lambda_handler(event, context):
     ec2 = boto3.resource('ec2', region_name=region)
     ec2RunningInstancesIDs = []
     instances = ec2.instances.filter(
+        # Filter Instances based On Tag, Check Current State
         Filters=[{'Name': 'tag:shutdown', 'Values': ['true']}, {'Name': 'instance-state-name', 'Values': ['running']}])
     if not instances:
         exit(1)
